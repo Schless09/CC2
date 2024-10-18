6c6e9a31051ca16a4d885d6860d8aea970b04673
Andrew Schuessler <schless09devtools@gmail.com>
2024-10-18 16:15:48 -0700

    updated cal.com

--- CC/src/components/CalEmbed.tsx
+++ CC/src/components/CalEmbed.tsx
@@ -5,12 +5,12 @@ import { useEffect } from "react";
 export default function () {
   useEffect(()=>{
     (async function () {
-      const cal = await getCalApi({"namespace":"15min"});
+      const cal = await getCalApi({"namespace":"20"});
       cal("ui", {"styles":{"branding":{"brandColor":"#000000"}},"hideEventTypeDetails":false,"layout":"month_view"});
     })();
   }, [])
-  return <Cal namespace="20min"
-    calLink="andrew-schuessler-xrp0oa/20min"
+  return <Cal namespace="20"
+    calLink="andrew-schuessler-xrp0oa/20"
     style={{width:"100%",height:"100%",overflow:"scroll"}}
     config={{"layout":"month_view"}}
   />;
