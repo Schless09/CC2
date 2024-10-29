5af3c2dc1fa10ccc3d32e7f6aa7a754629b907a4
Andrew Schuessler <schless09devtools@gmail.com>
2024-10-29 09:49:54 -0700

    favicon update

--- CC/src/components/HMItems.tsx
+++ CC/src/components/HMItems.tsx
@@ -20,7 +20,7 @@ const HMItems = () => {
       {/* Specialization Section */}
       <CarouselItem>
         <motion.section
-          className="relative specialization-section py-16 px-6 md:px-12 rounded-2xl bg-gradient-to-br from-pink-100 via-white to-purple-50 shadow-2xl overflow-hidden"
+          className="relative specialization-section py-16 px-6 md:px-12 rounded-2xl bg-gradient-to-br from-green-100 via-white to-green-50 shadow-2xl overflow-hidden"
           {...fadeIn}
         >
           <div className="absolute top-0 left-0 w-full h-full opacity-10 pointer-events-none">
@@ -35,7 +35,7 @@ const HMItems = () => {
             {areasOfExpertise.map((area, index) => (
               <motion.div
                 key={index}
-                className="expertise-item bg-gray-50 p-6 rounded-xl shadow-lg  hover:text-white transition-all duration-300"
+                className="expertise-item bg-gray-50 p-6 rounded-xl shadow-lg hover:text-white transition-all duration-300"
                 whileHover={hoverEffect.whileHover}
               >
                 <h4 className="font-semibold text-2xl mb-4 text-green">{area.name}</h4>
@@ -46,34 +46,33 @@ const HMItems = () => {
       </CarouselItem>
 
       {/* About Section */}
-        <CarouselItem>
-          <motion.section
-            className="about-section flex flex-col justify-center items-center py-10 md:py-16 px-4 md:px-12 rounded-xl h-full bg-gradient-to-br from-purple-100 via-white to-pink-50 shadow-2xl relative"
-            {...fadeIn}
-          >
-            {/* Background Image */}
-            <div
-              className="absolute inset-0 opacity-10 bg-cover bg-center"
-              style={{ backgroundImage: "url('/images/abstract-pattern.png')" }}
-            ></div>
+      <CarouselItem>
+        <motion.section
+          className="about-section flex flex-col justify-center items-center py-10 md:py-16 px-4 md:px-12 rounded-xl h-full bg-gradient-to-br from-green-100 via-white to-green-50 shadow-2xl relative"
+          {...fadeIn}
+        >
+          {/* Background Image */}
+          <div
+            className="absolute inset-0 opacity-10 bg-cover bg-center"
+            style={{ backgroundImage: "url('/images/abstract-pattern.png')" }}
+          ></div>
 
-            {/* Title */}
-            <h2 className="relative text-4xl md:text-6xl font-bold mb-6 md:mb-10 text-center text-transparent bg-clip-text bg-gradient-to-r from-green to-green2 drop-shadow-lg">
-              Building Teams at Every Level
-            </h2>
+          {/* Title */}
+          <h2 className="relative text-4xl md:text-6xl font-bold mb-6 md:mb-10 text-center text-transparent bg-clip-text bg-gradient-to-r from-green to-green2 drop-shadow-lg">
+            Building Teams at Every Level
+          </h2>
 
-            {/* Description */}
-            <p className="relative text-xl md:text-3xl leading-relaxed max-w-2xl md:max-w-4xl mb-6 md:mb-8 text-center text-gray-700">
-              Software Engineer | Senior Software Engineer | Staff Engineer | Engineering Manager | Director of Engineering
-            </p>
-          </motion.section>
-        </CarouselItem>
-
+          {/* Description */}
+          <p className="relative text-xl md:text-3xl leading-relaxed max-w-2xl md:max-w-4xl mb-6 md:mb-8 text-center text-gray-700">
+            Software Engineer | Senior Software Engineer | Staff Engineer | Engineering Manager | Director of Engineering
+          </p>
+        </motion.section>
+      </CarouselItem>
 
       {/* Locations Section */}
       <CarouselItem>
         <motion.section
-          className="locations-section flex flex-col justify-center items-center py-16 px-6 md:px-12 rounded-2xl h-full bg-gradient-to-r from-gray-50 to-pink-100 shadow-2xl relative"
+          className="locations-section flex flex-col justify-center items-center py-16 px-6 md:px-12 rounded-2xl h-full bg-gradient-to-r from-gray-50 to-green-100 shadow-2xl relative"
           {...fadeIn}
         >
           <div className="absolute top-0 left-0 w-full h-full opacity-10 pointer-events-none">
@@ -88,7 +87,7 @@ const HMItems = () => {
             {keyMarkets.map((city, index) => (
               <motion.div
                 key={index}
-                className="city-item bg-white p-6 rounded-xl shadow-md text-center hover:bg-pink-50 hover:shadow-lg transform transition duration-300"
+                className="city-item bg-white p-6 rounded-xl shadow-md text-center hover:bg-green-50 hover:shadow-lg transform transition duration-300"
                 whileHover={{ y: -5, scale: 1.1 }}
               >
                 <h4 className="font-semibold text-lg text-gray-700">{city}</h4>
@@ -101,10 +100,10 @@ const HMItems = () => {
       {/* Why Choose Us Section */}
       <CarouselItem>
         <motion.section
-          className="choose-us-section flex flex-col justify-center items-center py-16 px-6 md:px-12 rounded-2xl h-full bg-gradient-to-b from-purple-50 to-gray-50 shadow-2xl relative"
+          className="choose-us-section flex flex-col justify-center items-center py-16 px-6 md:px-12 rounded-2xl h-full bg-gradient-to-b from-green-50 to-gray-50 shadow-2xl relative"
           {...fadeIn}
         >
-          <div className="absolute inset-0 bg-gradient-to-t from-transparent to-pink-50 opacity-20 pointer-events-none"></div>
+          <div className="absolute inset-0 bg-gradient-to-t from-transparent to-green-50 opacity-20 pointer-events-none"></div>
 
           <h3 className="relative text-5xl font-semibold mb-12 text-center text-transparent bg-clip-text bg-gradient-to-r from-green to-green2">
             Why Choose Coder Collective?
