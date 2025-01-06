d84316cc20ab02f4b94421ed969c5afdefac443c
Andrew Schuessler <schless09devtools@gmail.com>
2025-01-06 13:07:43 -0800

    updated cursor

--- CC/src/components/landing-page/Hero.tsx
+++ CC/src/components/landing-page/Hero.tsx
@@ -1,10 +1,10 @@
-// components/landing-page/Hero.tsx
 'use client'
 import { useState, useEffect } from "react";
 import Link from "next/link";
 
 export default function Hero() {
   const [displayedText, setDisplayedText] = useState("");
+  const [isTyping, setIsTyping] = useState(true);
   const fullText = "Your network is the ticket in.";
 
   useEffect(() => {
@@ -16,6 +16,8 @@ export default function Hero() {
         setDisplayedText(fullText.slice(0, index + 1));
         index++;
         timeoutId = setTimeout(typeNextCharacter, 65);
+      } else {
+        setIsTyping(false);
       }
     };
 
@@ -37,7 +39,7 @@ export default function Hero() {
       <div className="absolute bottom-20 left-20 z-20">
         <h1 className="text-4xl sm:text-6xl font-bold mb-6 text-white">
           {displayedText}
-          <span className="animate-blink">|</span>
+          <span className={`${isTyping ? "" : "animate-blink"}`}>|</span>
         </h1>
         <Link href="/businessModel">
           <button className="px-6 py-3 bg-white hover:bg-gray-300 text-green font-semibold text-lg rounded-lg shadow-md transition">
--- CC/tailwind.config.ts
+++ CC/tailwind.config.ts
@@ -1,5 +1,6 @@
 import type { Config } from 'tailwindcss';
 import { withUt } from 'uploadthing/tw';
+
 const config = withUt({
   // darkMode: ['class'],
   content: [
@@ -77,10 +78,15 @@ const config = withUt({
           from: { height: 'var(--radix-accordion-content-height)' },
           to: { height: '0' },
         },
+        blink: {
+          '0%, 100%': { opacity: '1' },
+          '50%': { opacity: '0' },
+        },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
+        'blink': 'blink 1s step-end infinite',
       },
     },
   },
